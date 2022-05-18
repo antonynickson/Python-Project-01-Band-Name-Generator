be8590ddad2a8cf7e3205f1d3878c5911624b391
Antony Savarimuthu <antonynickson13@gmail.com>
2022-05-18 20:15:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#Welcome Message
+print("Welcome to Band generator Program")
+# Ask the user to enter the city they grew up
+user_city = input("Enter the city you grew up:\n")
+# sk the user the name of pet
+user_pet = input("Enter the name of your pet:\n")
+#Concat the string varaibles and print
+print("Your band name could be:", user_city + " " + user_pet)
